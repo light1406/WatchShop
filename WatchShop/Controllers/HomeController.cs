d022eaa0b870a0089f7e4883c93d1fbc0b08c236
Nguyen Minh Châu <91469379+light1406@users.noreply.github.com>
2022-01-12 14:54:06 +0700

    Add project files.

---
+++ WatchShop/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WatchShop.Models;
+
+namespace WatchShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DbWatchShop db = new DbWatchShop();
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Brand> rs = db.Brands.ToList();
+            return View(rs);
+        }
+    }
+}
